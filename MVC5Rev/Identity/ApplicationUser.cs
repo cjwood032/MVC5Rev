a18fb89899a2b2f32b90156f46296d76bb403fd2
Christopher Wood <cjwood032@gmail.com>
2021-03-26 09:43:24 -0400

    Identity created application user, store and manager

---
+++ MVC5Rev/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace MVC5Rev.Identity
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public DateTime? Birthday { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MVC5Rev/Identity/ApplicationUserStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace MVC5Rev.Identity
+{
+    public class ApplicationUserStore: UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext dbContext):base(dbContext)
+        {
+
+        }
+    }
+}
