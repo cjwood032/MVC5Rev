f4a38199acd7feab78e432a388ee46027fede161
Christopher Wood <cjwood032@gmail.com>
2021-03-26 12:19:43 -0400

    Configured manager and admin roles

--- MVC5Rev/Startup.cs
+++ MVC5Rev/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MVC5Rev.Identity;
+
 [assembly: OwinStartup(typeof(MVC5Rev.Startup))]
 
 namespace MVC5Rev
@@ -13,8 +15,52 @@ namespace MVC5Rev
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions() { AuthenticationType= DefaultAuthenticationTypes.ApplicationCookie, LoginPath=new PathString("/Account/Login")});
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() { AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Account/Login") });
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            this.CreateRolesAndUsers();
+        }
+        public void CreateRolesAndUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+            var appDbContext = new ApplicationDbContext();
+            var appUserStore = new ApplicationUserStore(appDbContext);
+            var userManager = new ApplicationUserManager(appUserStore);
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            if (roleManager.FindByName("Admin") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+                string userPassword = "admin123";
+                var chkUser = userManager.Create(user, userPassword);
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+            if (roleManager.FindByName("Manager") == null)
+            {
+                var mgrUser = new ApplicationUser();
+                mgrUser.UserName = "manager";
+                mgrUser.Email = "manager@gmail.com";
+                string mgrUserPassword = "manager123";
+                var mgrChkUser = userManager.Create(mgrUser, mgrUserPassword);
+                if (mgrChkUser.Succeeded)
+                {
+                    userManager.AddToRole(mgrUser.Id, "Manager");
+                }
+            }
         }
     }
 }
